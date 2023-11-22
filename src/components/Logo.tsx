3ea9cb14f7c782f7e84f18e1be40233faa7a2bdc
RuthBiney <r.biney@alutstudent.com>
2023-11-22 23:32:03 +0200

    edit the logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,16 @@
 const Logo = () => {
   return (
-    <h2 className="font-bold text-4xl border rounded-full py-2 px-6 shadow-sm border-red-500">
-      <span className="text-orange-500">WI</span>
-      <span className="text-orange-900">T</span>
-    </h2>
+    <div className="flex items-center space-x-2 border rounded-full py-2 px-6 shadow-lg bg-orange-50">
+      <h2 className="font-bold text-4xl ">
+        <span className="text-orange-500">WI</span>
+        <span className="text-orange-900">T</span>
+      </h2>
+      <p className="text-orange-900 font-semibold">
+        <span className="text-orange-500 font-bold text-lg">W</span>omen{" "}
+        <span className="text-orange-500 font-bold text-lg">I</span>n{" "}
+        <span className="text-orange-500 font-bold text-lg">T</span>ech
+      </p>
+    </div>
   );
 };
 
--- src/routing/Layout.tsx
+++ src/routing/Layout.tsx
@@ -4,7 +4,7 @@ import NavBar from "../components/NavBar";
 const Layout = () => {
   return (
     <>
-      <header className="border-b-2 py-4 px-24 shadow-md">
+      <header className="border-b-2 py-5 px-24 shadow-md rounded-xl">
         <NavBar />
       </header>
       <div className="bg-gray-100 h-screen py-4 px-10">
