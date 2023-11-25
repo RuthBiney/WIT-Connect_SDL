a8484fb8a2616278d01039795224bd0bbf23714f
RuthBiney <r.biney@alutstudent.com>
2023-11-25 22:23:18 +0200

    refactor: remove unused useEffect

--- src/components/HamburgerIcon.tsx
+++ src/components/HamburgerIcon.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Hamburger from "hamburger-react";
 import { Link } from "react-router-dom";
 import { aboutUs, contact, signIn } from "../utils/links";
